2210d6cd51a959d9fce8e0f7a30511d4b2062ccb
ssbeast <ssbeast007@gmail.com>
2020-08-08 17:14:27 +0530

    Card js added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Card />,
   document.getElementById('root')
 );
 
