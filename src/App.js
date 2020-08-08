35df6ef7cc79770672704585ae9082fe6c4deaeb
ssbeast <ssbeast007@gmail.com>
2020-08-08 16:57:41 +0530

    tachyons imported

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <div className="App">
@@ -26,3 +26,4 @@ class App extends React.Component {
 }
 
 export default App;
+ 
