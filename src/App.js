cf83d84453eb7e0420b90987e628a4486064f6f4
ssbeast <ssbeast007@gmail.com>
2020-08-09 15:45:59 +0530

    componentdidmount added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots : robots,
+            robots : [],
             searchfield : ''
         }
+    }
+
+    componentDidMount() {
+        this.setState({ robots: robots });
     }
 
     onSearchChange = (event) => {
