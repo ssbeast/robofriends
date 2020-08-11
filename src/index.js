1f9356562dde8c3ae39e8bd3242a0c84e643b021
ssbeast <ssbeast007@gmail.com>
2020-08-11 21:07:35 +0530

    store added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux'; 
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { searchRobots } from './reducers';
+
+const store = createStore(searchRobots)
+
 
 ReactDOM.render(
-  <App />,
+  <App store={store} />,
   document.getElementById('root')
 );
 
