37389e374157b42b65d48c94fdcc60988a4e228a
ssbeast <ssbeast007@gmail.com>
2020-11-28 19:24:37 +0530

    thunk added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux'; 
+import { applyMiddleware, createStore } from 'redux'; 
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { searchRobots } from './reducers';
 import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 
 const logger = createLogger() 
 
-const store = createStore(searchRobots)
+const store = createStore(searchRobots, applyMiddleware(thunkMiddleware, logger))
 
 
 ReactDOM.render(
