c5ba1d562608fc54489e36a39e5381f6db06a10d
ssbeast <ssbeast007@gmail.com>
2020-11-28 19:41:56 +0530

    reducers combined  added

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux'; 
+import { applyMiddleware, createStore, combineReducers} from 'redux'; 
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 
-const logger = createLogger() 
+const logger = createLogger();
 
-const store = createStore(searchRobots, applyMiddleware(thunkMiddleware, logger))
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
 
 
 ReactDOM.render(
